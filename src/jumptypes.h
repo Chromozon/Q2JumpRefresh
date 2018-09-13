af29478b855333cf9f34fe0a3e11c3449edf42ea
Chromozon <Chromozon@users.noreply.github.com>
2018-09-13 18:59:54 -0500

    Added jumptypes.h header
    This header defines data types that have no other dependencies.

---
+++ src/jumptypes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Data types that do not have any dependencies on any other headers
+
+namespace Jump
+{
+
+    typedef enum {
+        TEAM_NONE,
+        TEAM_EASY,
+        TEAM_HARD
+    } team_t;
+
+}

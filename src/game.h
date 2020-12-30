34ee343b94b359169c78d1bea1ddb249bfe70556
Chromozon <Chromozon@users.noreply.github.com>
2020-12-29 20:32:02 -0600

    Added a comment to look at adding "#pragma once" to game.h header file.

--- src/game.h
+++ src/game.h
@@ -18,6 +18,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
+// TODO!!
+//#pragma once
+
 // game.h -- game dll information visible to server
 
 #define	GAME_API_VERSION	3

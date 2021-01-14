a0af254d327e448247101e8f11224fa61bd6c116
Chromozon <Chromozon@users.noreply.github.com>
2021-01-13 22:22:34 -0600

    Changed from using a raw TCP socket to using HTTP.  The rational is that HTTP already has mechanisms to send large blobs of data (for replays) so we won't have to reinvent the wheel.

--- jumpdatabase/Program.cs
+++ jumpdatabase/Program.cs
@@ -3,8 +3,10 @@ using Newtonsoft.Json;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace jumpdatabase
 {
@@ -34,68 +36,95 @@ namespace jumpdatabase
         }
 
         const int ServicePort = 57540;
+        const string DatabasePath = "./jumpdatabase.sqlite3";
 
         static void Main(string[] args)
         {
-            TcpListener tcpListener = new TcpListener(IPAddress.Any, ServicePort);
-            tcpListener.Start();
-            Console.WriteLine($"Q2 Jump Database service listening on port {ServicePort}");
+            // Initialize the database
+            SqliteConnection dbConnection = new SqliteConnection($"Data Source={DatabasePath}");
+            dbConnection.Open();
+            Console.WriteLine($"Opened connection to database \"{DatabasePath}\"");
+            Tables.CreateAllTables(dbConnection);
 
-            byte[] buffer = new byte[1024];
+            // Start listening for requests from the various servers
+            HttpListener httpListener = new HttpListener();
+            httpListener.Prefixes.Add($"http://localhost:{ServicePort}/");
+            httpListener.Start();
+            Console.WriteLine($"Q2 Jump Database service listening on port {ServicePort}");
 
             while (true)
             {
                 try
                 {
-                    TcpClient client = tcpListener.AcceptTcpClient();
-                    NetworkStream stream = client.GetStream();
-                    do
-                    {
-                        int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                        string jsonStr = System.Text.Encoding.ASCII.GetString(buffer);
-                        dynamic jsonObj = JsonConvert.DeserializeObject(jsonStr);
+                    // Receive a request from a server
+                    var context = httpListener.GetContext();
+                    var request = context.Request;
+                    var response = context.Response;
 
-                        string token = jsonObj.token;
-                        if (token == null)
-                        {
-                            Console.WriteLine("Invalid json: no token");
-                            break;
-                        }
-                        bool validToken = _serverTokens.TryGetValue(token, out var serverName);
-                        if (!validToken)
-                        {
-                            Console.WriteLine("Invalid token");
-                            break;
-                        }
-                        string command = jsonObj.command;
-                        if (command == null)
-                        {
-                            Console.WriteLine("Invalid json: no command");
-                            break;
-                        }
-                        dynamic commandArgs = jsonObj.commandargs;
-                        if (commandArgs == null)
-                        {
-                            Console.WriteLine("Invalid json: no command args");
-                            return;
-                        }
-                        if (command == "addtime")
-                        {
-                            HandleCommandAddTime(commandArgs);
-                        }
-                        else if (command == "gettimes")
-                        {
-                            HandleCommandGetTimes(commandArgs);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid command");
-                            break;
-                        }
+                    // Parse the request
+                    if (request.ContentType != "application/json")
+                    {
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        response.Close();
+                        continue;
                     }
-                    while (stream.DataAvailable);
-                    stream.Close();
-                    client.Close();
+                    MemoryStream memoryStream = new MemoryStream();
+                    request.InputStream.CopyTo(memoryStream);
+                    memoryStream.Position = 0;
+                    dynamic json = JsonConvert.DeserializeObject(Encoding.ASCII.GetString(memoryStream.ToArray()));
+
+                    string loginToken = json.login_token;
+                    if (loginToken == null)
+                    {
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        response.Close();
+                        continue;
+                    }
+                    int serverId = -1;
+                    if (!_servers.TryGetValue(loginToken, out serverId))
+                    {
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        response.Close();
+                        continue;
+                    }
+                    string command = json.command;
+                    if (string.IsNullOrEmpty(command))
+                    {
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        response.Close();
+                        continue;
+                    }
+                    dynamic commandArgs = json.command_args;
+                    if (commandArgs == null)
+                    {
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        response.Close();
+                        continue;
+                    }
+
+                    // Execute the command
+                    int responseStatus = (int)HttpStatusCode.BadRequest;
+                    string responseData = string.Empty;
+                    switch (command)
+                    {
+                        case "addtime":
+                            HandleCommandAddTime(serverId, commandArgs, out responseStatus, out responseData);
+                            break;
+                        case "gettimes":
+                            HandleCommandGetTimes(serverId, commandArgs, out responseStatus, out responseData);
+                            break;
+                        case "userlogin":
+                            break;
+                        default:
+                            break;
+                    }
+
+                    // Send response back to the server
+                    response.StatusCode = responseStatus;
+                    var outputStream = response.OutputStream;
+                    outputStream.Write(Encoding.ASCII.GetBytes(responseData));
+                    outputStream.Close();
+                    response.Close();
                 }
                 catch (Exception e)
                 {
@@ -104,8 +133,25 @@ namespace jumpdatabase
             }
         }
 
-        static private void HandleCommandAddTime(dynamic args)
+        /// <summary>
+        /// Adds a new best time for the given user and map.
+        /// We ensure that the new time is faster than whatever is currently in the database.
+        /// </summary>
+        /// <param name="serverId"></param>
+        /// <param name="args">
+        /// {
+        ///     "user": "username" (string)
+        ///     "date": 1610596223836 (int, Unix time ms)
+        ///     "time_ms": 234934 (int, ms)
+        /// }
+        /// </param>
+        /// <param name="status"></param>
+        /// <param name="data"></param>
+        static private void HandleCommandAddTime(int serverId, dynamic args, out int status, out string data)
         {
+            status = (int)HttpStatusCode.BadRequest;
+            data = string.Empty;
+
             string user = args.user;
             long? date = args.date;
             long? time_ms = args.time_ms;
@@ -127,36 +173,49 @@ namespace jumpdatabase
             // TODO: update database, when adding, check if time is faster
         }
 
-        static private List<TimeRecord> HandleCommandGetTimes(dynamic args)
+        /// <summary>
+        /// Retrieves a set of times for the given map.
+        /// </summary>
+        /// <param name="serverId"></param>
+        /// <param name="args">
+        /// {
+        ///     "mapname": "mapname" (string)
+        ///     "page": 1 (int, 1-based)
+        /// }
+        /// </param>
+        /// <param name="status"></param>
+        /// <param name="data"></param>
+        static private void HandleCommandGetTimes(int serverId, dynamic args, out int status, out string data)
         {
-            List<TimeRecord> times = new List<TimeRecord>();
+            status = (int)HttpStatusCode.BadRequest;
+            data = string.Empty;
+
             string mapname = args.mapname;
             int? page = args.page;
-            if (mapname == null)
+            if (mapname == null || page == null)
             {
-                Console.WriteLine("Invalid gettimes command: no mapname");
-                return times;
-            }
-            if (page == null)
-            {
-                Console.WriteLine("Invalid gettimes command: no page");
-                return times;
+                return;
             }
             if (page.Value < 1)
             {
                 page = 1;
             }
+            List<TimeRecord> times = new List<TimeRecord>();
             // query database for maptimes at page n
-            return times;
         }
 
-        
-
-        static private Dictionary<string, string> _serverTokens = new Dictionary<string, string>()
+        // Cache the server login tokens -> serverid
+        static private Dictionary<string, int> _servers = new Dictionary<string, int>()
         {
-            { "UyzyfBHakVoJM2sy", "German All" },
-            { "N5WgTrdyd6TDmQby", "German Race" },
-            { "DdBsAfhmpwEKHgnO", "US All" },
+            { "UyzyfBHakVoJM2sy", 0 }, // German
+            { "N5WgTrdyd6TDmQby", 1 }, // German 2
+            { "DdBsAfhmpwEKHgnO", 2 }, // US
         };
+
+        // Cache all of the usernames -> userid
+        static private Dictionary<string, int> _users = new Dictionary<string, int>();
+
+        // Cache of all mapnames -> mapid
+        static private Dictionary<string, int> _maps = new Dictionary<string, int>();
     }
 }

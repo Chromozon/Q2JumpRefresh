0ab0ee5eb76dc3c0a7ab39dc915288242b4c521b
Chromozon <Chromozon@users.noreply.github.com>
2021-04-10 22:39:11 -0500

    Added skinnum to ent parsing code so that jumpboxes get set automatically from the ent files.

--- jumpserver/g_save.c
+++ jumpserver/g_save.c
@@ -54,6 +54,7 @@ field_t fields[] = {
 	{"volume", FOFS(volume), F_FLOAT},
 	{"attenuation", FOFS(attenuation), F_FLOAT},
 	{"map", FOFS(map), F_LSTRING},
+	{"skinnum", FOFS(s.skinnum), F_INT},
 
 	// temp spawn vars -- only valid when the spawn function is called
 	{"lip", STOFS(lip), F_INT, FFL_SPAWNTEMP},

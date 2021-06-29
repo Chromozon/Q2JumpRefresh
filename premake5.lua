cfde41251fe6482ce162977647cca2546fa428e4
Miikka Ylätalo <totally.mehis@gmail.com>
2021-06-29 23:37:48 +0300

    Add q2path, q2exe and q2jumpdir premake commandline arguments
    The usage should now be: premake5 --q2path="C:/Quake2/" --q2exe="q2pro.exe"

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,43 @@
+DEFAULT_GAME_FOLDER = "jumprefresh"
+
+--[[
+Command line arguments
+--]]
+newoption {
+    trigger = "q2path",
+    description = "Path to Quake 2 client.",
+    value = "C:\\Games\\Quake2\\",
+    default = ""
+}
+
+newoption {
+    trigger = "q2exe",
+    description = "Quake 2 client executable name.",
+    value = "q2pro.exe",
+    default = "q2pro.exe"
+}
+
+newoption {
+    trigger = "q2args",
+    description = "Quake 2 client arguments.",
+    value = "+game " .. DEFAULT_GAME_FOLDER,
+    default = "+game " .. DEFAULT_GAME_FOLDER
+}
+
+newoption {
+    trigger = "q2jumpdir",
+    description = "Quake 2 jump folder name. Defaults to " .. DEFAULT_GAME_FOLDER,
+    default = DEFAULT_GAME_FOLDER
+}
+
+-- Append slash to the end of Quake 2 path.
+if _OPTIONS["q2path"] then
+    local lastchar = string.sub(_OPTIONS["q2path"], #_OPTIONS["q2path"])
+    if lastchar ~= "/" and lastchar ~= "\\" then
+        _OPTIONS["q2path"] = _OPTIONS["q2path"] .. "/"
+    end
+end
+
 workspace "jump"
     configurations { "Debug", "Release" }
 
@@ -13,6 +53,9 @@ project "jumpserver"
     cppdialect "C++17"
     targetname "gamex86"
     architecture "x86"
+
+    debugcommand (_OPTIONS["q2path"] .. _OPTIONS["q2exe"])
+    debugargs { _OPTIONS["q2args"] }
 
     files { "jumpserver/**.h", "jumpserver/**.c", "jumpserver/**.cpp" }
 
@@ -42,6 +85,14 @@ project "jumpserver"
         disablewarnings "write-strings"
     filter "system:Windows"
         disablewarnings "4996"
+
+    -- Post-build commands
+    filter "system:Windows"
+        -- Move game library + debug symbols to game folder.
+        postbuildcommands {
+            "copy /B /Y \"$(OutDir)$(TargetFileName)\" \"" .. _OPTIONS["q2path"] .. _OPTIONS["q2jumpdir"] .. "\"",
+            "copy /B /Y \"$(OutDir)$(TargetName).pdb\" \"" .. _OPTIONS["q2path"] .. _OPTIONS["q2jumpdir"] .. "\""
+        }
 
 --[[
 Jump Database
